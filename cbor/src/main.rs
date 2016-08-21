ba13e70f8a3b5a3b9d065d5fd6cdcc65f562007a
yuta <yu.ichiko@gmail.com>
2016-08-22 00:39:51 +0900

    Add cbor

--- cbor/src/main.rs
+++ cbor/src/main.rs
@@ -8,7 +8,7 @@ use cbor::{Decoder, Encoder};
 
 fn main() {
     let path = Path::new("/Users/yuta/IdeaProjects/rust-study/cbor/src/cbor.txt");
-    let data = vec![('a', 1), ('b', 2), ('c', 3)];
+    let data = vec![('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)];
 
     let mut e = Encoder::from_memory();
     e.encode(&data).unwrap();
@@ -16,15 +16,20 @@ fn main() {
     let mut wfile = File::create(&path).unwrap();
     wfile.write_all(e.as_bytes()).unwrap();
 
-    let mut rfile = File::open(&path).unwrap();
-    rfile.seek(SeekFrom::Start(8)).unwrap();
+    let wbuf: [u8; 4] = [130, 24, 120, 20];
+    wfile.seek(SeekFrom::Start(4)).unwrap();
+    wfile.write(&wbuf).unwrap();
+    println!("CBOR::Encoder {:?}", wbuf);
 
-    let mut buffer = [0; 4];
+    let mut rfile = File::open(&path).unwrap();
+    rfile.seek(SeekFrom::Start(4)).unwrap();
+
+    let mut buffer: [u8; 4] = [0; 4];
     rfile.read(&mut buffer);
     println!("The bytes: {:?}", buffer);
 
-    let slice: &[u8] = &buffer;
-    let mut d = Decoder::from_bytes(slice);
+    //let slice: &[u8] = &buffer;
+    let mut d = Decoder::from_bytes(&buffer[0..4]);
     let items: Vec<(char, i32)> = d.decode().collect::<Result<_, _>>().unwrap();
     println!("CBOR::Decoder {:?}", items);
 }
